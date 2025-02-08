834d0983029f1fad794daa2e9fd14953953f2713
bakursky <rrrrrrrrush@gmail.com>
2025-02-08 17:09:28 +0300

    minor variable fixes for run build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono,  Press_Start_2P} from "next/font/google";
+import { Press_Start_2P} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 const pressStart = Press_Start_2P({
   weight: "400",
